c1afc60592ab3a35d905c669a159cb800b321d1a
Kelsi Olstad <kelsiolstad@gmail.com>
2022-03-22 20:47:36 -0600

    pass data to each DayListItem

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-export default function DayList() {
+export default function DayList(props) {
+
+  const dayListComponents = props.days.map(day => {
+    return (
+      <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={props.name === day.name}
+      setDay={day.setDay}
+      />
+    );
+  });
+
   return (
-    <ul></ul>
-  )
-}
+    <ul> {dayListComponents} </ul>
+  );
+};
