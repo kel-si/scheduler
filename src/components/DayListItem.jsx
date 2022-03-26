9ff080de630889ec87db7268064eb7865ab77e8f
Kelsi Olstad <kelsiolstad@gmail.com>
2022-03-26 16:10:46 -0600

    use availability

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import classNames from 'classnames';
+import classNames from "classnames";
 import "components/DayListItem.scss";
 
 const formatSpots = (spots) => {
@@ -13,15 +13,18 @@ const formatSpots = (spots) => {
 };
 
 export default function DayListItem(props) {
-  
-  const dayClass = classNames('day-list__item', {'day-list__item--selected': props.selected}, {'day-list__item--full': !props.spots});
+  const dayClass = classNames(
+    "day-list__item",
+    { "day-list__item--selected": props.selected },
+    { "day-list__item--full": !props.spots }
+  );
 
   const availability = formatSpots(props.spots);
 
   return (
     <li onClick={props.setDay} className={dayClass}>
-      <h2 className="text--regular" >{props.name}</h2>
-      <h3 className="text--light" >{props.spots} spots remaining</h3>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{availability} </h3>
     </li>
   );
 }
