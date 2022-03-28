c61c1aa63d52759c2f47bef1c5856e40d5a39a1e
Kelsi Olstad <kelsiolstad@gmail.com>
2022-03-28 17:22:01 -0600

    refactor; add replace

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,25 +1,30 @@
+import { FALSE } from "node-sass";
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  //mode is the last item in the history array
   const [mode, setMode] = useState(initial);
-  const [history, setHistory] = useState([]);
+  const [history, setHistory] = useState([initial]);
 
-  function transition(newMode) {
-    setHistory((prev) => [...prev, mode]);
+  function transition(newMode, replace = false) {
+    if (replace) {
+      const newHistory = [...history];
+      newHistory[newHistory.length - 1] = newMode;
+      setHistory(newHistory);
+    } else {
+      setHistory((prev) => [...prev, newMode]);
+    }
     setMode(newMode);
   }
 
   //transition back to previous mode
   function back() {
     let historyCopy = history.slice(0);
-    if (historyCopy.length >= 1) {
+    if (historyCopy.length > 1) {
       const prevMode = historyCopy.pop();
-      setMode(prevMode);
+      const secondLastElement = history.length - 2;
+      setMode(historyCopy[secondLastElement]);
       setHistory(historyCopy);
-      return mode;
     }
-    return historyCopy;
   }
 
   return { mode, transition, back };
