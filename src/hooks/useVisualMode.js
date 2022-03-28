397c260dbb7ee0814b7e4acf5bd506f8a7223608
Kelsi Olstad <kelsiolstad@gmail.com>
2022-03-27 20:32:39 -0600

    create transition function and test

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,6 +1,13 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-  return { mode };
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode) {
+    setHistory((prev) => [newMode, ...prev]);
+    setMode(newMode);
+  }
+
+  return { mode, transition };
 }
