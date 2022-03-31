957b1fb19d05f0d86cb5d90180838b9fafe76c0f
Kelsi Olstad <kelsiolstad@gmail.com>
2022-03-30 23:28:56 -0600

    remove console.log

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -12,7 +12,6 @@ export default function useApplicationData() {
   const setDay = (day) => setState({ ...state, day });
 
   function bookInterview(id, interview) {
-    console.log("interview", interview);
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview },
